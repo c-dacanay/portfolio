c037b3079c7c0beb6251d5b4ed336825766902ee
c-dacanay <tina.dacanay@gmail.com>
2023-07-27 22:34:37 -0400

    nice backgrounds and uh content I guess

---
+++ design/lil.js
@@ -0,0 +1,12 @@
+console.log('hewwo')
+
+const root = document.documentElement;
+ 
+// change object
+document.addEventListener('mousemove', evt => {
+    let x = evt.clientX / innerWidth;
+    let y = evt.clientY / innerHeight;
+ 
+    root.style.setProperty('--mouse-x', x);
+    root.style.setProperty('--mouse-y', y);
+});
